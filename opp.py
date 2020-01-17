b73aff4b6fb4e4341ac90c9551cb29c4e0f2174b
Emilio Araos <ea3dev@gmail.com>
2020-01-16 19:41:08 -0700

    examples

--- opp.py
+++ opp.py
@@ -134,5 +134,83 @@ def pet_speak(pet):
 pet_speak(niko)
 pet_speak(felix)
 
+class Animal():
+
+	def __init__(self, name):
+		self.name: name
+
+	#def speak(self):
+		#raise NotImplementedError("Subclass must implement this abstract method")
+
+#myanimal2 = Animal('Fred')
+#myanimal2.speak()
+
+##################### Magic methods// dunder mthods.
+
+mylist = [1,2,3]
+print(len(mylist))
+
+class Sample():
+	pass
+
+mysample= Sample()
+
+print(mysample)
+print(mylist)
+
+class Book():
+
+	def __init__(self,title, author, pages):
+		self.title = title
+		self.author = author
+		self.pages = pages
+
+	def __str__(self):
+		return f"{self.title} by {self.author} "
+
+	def __len__(self):
+		return self.pages
+
+	def __del__(self):
+		print("A book object has been deleted")
+
+b = Book('Python Rocks', 'Emilio', 200)
+print(b)
+
+print(str(b))
+
+print(len(b))
+
+
+del b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
