631d94aeb880d9d475f542aadb0637f4faf0e9b1
Emilio Araos <ea3dev@gmail.com>
2020-01-17 23:15:24 -0700

    fixed methods

--- bank.py
+++ bank.py
@@ -1,4 +1,4 @@
-class Account:
+class Account():
 
 	
 	def __init__(self,owner,balance):
@@ -10,7 +10,9 @@ class Account:
 
 	def deposit(self, amount_to_deposit):
 
-		self.balance = self.balance + amount
+		self.balance = self.balance + amount_to_deposit
+
+		return f"You deposited {amount_to_deposit}, your new balance is: {self.balance}"
 
 
 
@@ -18,13 +20,13 @@ class Account:
 
 		if (amount_to_withdraw > self.balance):
 
-			return "Not enough money to withdraw"
+			return f"Not enough money to withdraw. Your balance is: {self.balance}"
 
 		else:
 
 			self.balance = self.balance - amount_to_withdraw
 
-			return f"Your new balance is: {self.balance}"
+			return f"Amount withdraw was: {amount_to_withdraw}. Your new balance is: {self.balance}"
 
 	def __str__(self):
 		return f"{self.owner} has {self.balance} available"
@@ -32,6 +34,14 @@ class Account:
 
 
 account_1 = Account("Emilio", 1000000)
+
+print(str(account_1))
+print(account_1.deposit(500000))
+print(account_1.withdraw(2000000))
+print(account_1.withdraw(1500000))
+
+
+
 
 
 
